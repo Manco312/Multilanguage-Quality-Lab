62a8ef8773376bd74ec0f3e6fbfd99eddc3618f5
Santiago Manco <129436458+Manco312@users.noreply.github.com>
2026-03-04 15:00:59 +0000

    Correction: two tests files

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -1,30 +1,8 @@
-
-# TODO: Escribe pruebas unitarias para es_palindromo y suma.
-# Sugerencias:
-# - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
-# - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
-
-from python_app.palindromo import es_palindromo
 from python_app.utils import suma
 
 
 def test_ejemplo_siembra():
     assert True
-
-def test_radar_palindromo():
-    assert es_palindromo("radar")
-
-def test_Radar_palindromo():
-    assert es_palindromo("Radar")
-
-def test_anita_palindromo():
-    assert es_palindromo("anita lava la tina")
-
-def test_python_palindromo():
-    assert not es_palindromo("python")
-
-def test_empty_palindromo():
-    assert es_palindromo("")
 
 def test_suma_positivos():
     assert suma(2, 3) == 5
---
+++ python_app/tests/test_palindromo.py
@@ -0,0 +1,17 @@
+from python_app.palindromo import es_palindromo
+
+
+def test_radar_palindromo():
+    assert es_palindromo("radar")
+
+def test_Radar_palindromo():
+    assert es_palindromo("Radar")
+
+def test_anita_palindromo():
+    assert es_palindromo("anita lava la tina")
+
+def test_python_palindromo():
+    assert not es_palindromo("python")
+
+def test_empty_palindromo():
+    assert es_palindromo("")
