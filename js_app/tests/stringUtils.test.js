5d6c9af2bc325699e60fbb9f9682cb307dde1ca1
Santiago Manco <129436458+Manco312@users.noreply.github.com>
2026-03-04 15:03:45 +0000

    Test file completed

--- js_app/tests/stringUtils.test.js
+++ js_app/tests/stringUtils.test.js
@@ -1,7 +1,21 @@
+const { isPalindrome } = require('../src/stringUtils');
 
-// TODO: Escribe pruebas unitarias para isPalindrome.
-// Sugerencias: "radar" -> true; "anita lava la tina" -> true; "python" -> false; "" -> true; "Radar" -> true
+test('radar_palindromo', () => {
+  expect(isPalindrome('radar')).toBe(true);
+});
 
-test('ejemplo siembra', () => {
-  expect(true).toBe(true);
+test('Radar_palindromo', () => {
+  expect(isPalindrome('Radar')).toBe(true);
+});
+
+test('anita_palindromo', () => {
+  expect(isPalindrome('anita lava la tina')).toBe(true);
+});
+
+test('python_palindromo', () => {
+  expect(isPalindrome('python')).toBe(false);
+});
+
+test('empty_palindromo', () => {
+  expect(isPalindrome('')).toBe(true);
 });
