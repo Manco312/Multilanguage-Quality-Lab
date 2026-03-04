5aff53bf99e562b9e90146e49006765f1661ff70
Santiago Manco <129436458+Manco312@users.noreply.github.com>
2026-03-04 14:53:51 +0000

    Tests for palindromo, utils and Ruff corrections

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -4,8 +4,33 @@
 # - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
 # - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
 
-from palindromo import es_palindromo
-from utils import suma
+from python_app.palindromo import es_palindromo
+from python_app.utils import suma
+
 
 def test_ejemplo_siembra():
     assert True
+
+def test_radar_palindromo():
+    assert es_palindromo("radar")
+
+def test_Radar_palindromo():
+    assert es_palindromo("Radar")
+
+def test_anita_palindromo():
+    assert es_palindromo("anita lava la tina")
+
+def test_python_palindromo():
+    assert not es_palindromo("python")
+
+def test_empty_palindromo():
+    assert es_palindromo("")
+
+def test_suma_positivos():
+    assert suma(2, 3) == 5
+
+def test_suma_cero():
+    assert suma(0, 5) == 5
+
+def test_suma_negativo():
+    assert suma(-2, 3) == 1
